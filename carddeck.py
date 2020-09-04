5864ab921dd5a5ec8b5f006c00e9f20753445ca0
Ryan Weisner <36051739+rjweis@users.noreply.github.com>
2020-09-04 14:09:35 +0000

    remove dead method, update docstring

--- carddeck.py
+++ carddeck.py
@@ -1,7 +1,13 @@
 from card import Card
 
 class CardDeck:
+    '''Players get their cards from the card deck.'''
     def get_cards(self):
+        '''Card deck will be defined as list of card objects. Each
+        card object is represented by a string, where the 0th char
+        represents the suit and the remaining chars represent the
+        rank.
+        '''
         suits = ['S', 'C', 'D', 'H']
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         cards = []
@@ -13,6 +19,3 @@ class CardDeck:
 
     def __init__(self):
         self.cards = self.get_cards()
-
-    def number_of_cards(self):
-        return(len(self.cards))
